658f0ed2c3c57ab1c9884d324100a99a82c46c78
daniel <dlevi309@gmail.com>
2022-04-29 23:04:00 -0700

    update README.md and clean up dloader.m

--- dloader.m
+++ dloader.m
@@ -6,33 +6,31 @@ extern const char *__progname;
 char dylibs_[800][800];
 int count_dylibs_;
 
-static void dloader_make_array() {
-	@autoreleasepool {
-		@try {
-			NSString *yourFolderPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"dloader"];
-			if (access(yourFolderPath.UTF8String, F_OK) != 0) {
-			}
-			NSArray  *array_list = [[[NSFileManager defaultManager] contentsOfDirectoryAtPath:yourFolderPath error:nil]?:[NSArray array] copy];
-			array_list = [array_list filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"SELF ENDSWITH %@", @"dylib"]];
-			array_list = [array_list sortedArrayUsingSelector:@selector(compare:)];
-			count_dylibs_ = [array_list count];
-			for (int i=0; i<count_dylibs_; i++) {
-				strcpy(dylibs_[i], [yourFolderPath stringByAppendingPathComponent:[array_list objectAtIndex:i]?:@""].UTF8String);
-			}
-		} @catch(NSException* e) {
-			
-		}
-	}
-}
-
-__attribute__((constructor))
-static void dloader()
+static void dloader_make_array()
 {
-	dloader_make_array();
-	for (int i=0; i<count_dylibs_; i++) {
-		if (access(dylibs_[i], F_OK) == 0) {
-			dlopen(dylibs_[i], RTLD_LAZY | RTLD_GLOBAL);
-		}
-	}
+    @autoreleasepool {
+        @try {
+            NSString *yourFolderPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"dloader"];
+            if (access(yourFolderPath.UTF8String, F_OK) != 0) {
+            }
+            NSArray *array_list = [[[NSFileManager defaultManager] contentsOfDirectoryAtPath:yourFolderPath error:nil] ?: [NSArray array] copy];
+            array_list = [array_list filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"SELF ENDSWITH %@", @"dylib"]];
+            array_list = [array_list sortedArrayUsingSelector:@selector(compare:)];
+            count_dylibs_ = [array_list count];
+            for (int i = 0; i < count_dylibs_; i++) {
+                strcpy(dylibs_[i], [yourFolderPath stringByAppendingPathComponent:[array_list objectAtIndex:i] ?: @""].UTF8String);
+            }
+        } @catch (NSException *e) {
+        }
+    }
 }
 
+__attribute__((constructor)) static void dloader()
+{
+    dloader_make_array();
+    for (int i = 0; i < count_dylibs_; i++) {
+        if (access(dylibs_[i], F_OK) == 0) {
+            dlopen(dylibs_[i], RTLD_LAZY | RTLD_GLOBAL);
+        }
+    }
+}
